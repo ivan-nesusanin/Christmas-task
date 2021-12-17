10c77480f99d1a804944c26c9f7f53e3f4b46a20
ivan-nesusanin <l75x5@yandex.ru>
2021-12-17 16:40:31 +0300

    feat: add the header in the application. Add new components home, toys, tree. Сonfigure routing

--- christmas-task/src/app/app-routing.module.ts
+++ christmas-task/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ToysComponent } from './toys/toys.component';
+import { TreeComponent } from './tree/tree.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'toys', component: ToysComponent },
+  { path: 'tree', component: TreeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- christmas-task/src/app/app.module.ts
+++ christmas-task/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SettingsComponent } from './toys/settings/settings.component';
+import { ToyComponent } from './toys/toy/toy.component';
+import { ToysComponent } from './toys/toys.component';
+import { HomeComponent } from './home/home.component';
+import { TreeComponent } from './tree/tree.component';
+import { CardComponent } from './toys/toy/card/card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SettingsComponent, ToyComponent, ToysComponent, HomeComponent, TreeComponent, CardComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
